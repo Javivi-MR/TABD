59ff0167b720c1bef0b9e22e946506d72ba4682e
Francisco Javier Molina Rojas <99264371+Javivi-MR@users.noreply.github.com>
2023-03-07 17:08:12 +0100

    Practica2 finalizada - A falta de revisión global

--- Practica 2/Ejercicio1.sql
+++ Practica 2/Ejercicio1.sql
@@ -1,10 +1,8 @@
---Autores: Adriana Maña Watson | Francisco Javier Molina Rojas
+--Autores: Francisco Javier Molina Rojas| Adriana Maña Watson
+
+SET SERVEROUTPUT ON; -- Habilitamos el output
 
 -- <------ Definicion de las Tablas ------> --
-
-SET SERVEROUTPUT ON;
-
-
 CREATE TABLE CUENTAS  --Creamos tabla CUENTAS
 (
     IDCuenta NUMBER(4), --IDCuenta es un numero de 4 cifras como mucho
@@ -28,75 +26,122 @@ CREATE TABLE ACCIONES
 
 DECLARE
 -- <- Variables -> --
-    IDC NUMBER(4) := &id;
-    Operacion CHAR(1) := '&op';
-    ValorNuevo NUMBER(11,2) := &vn;
-    Comentario VARCHAR(45) := '&c';
+    IDC NUMBER(4) := &id;   -- Introducimos id 
+    Operacion CHAR(1) := '&op';  -- Introducimos tipo de operacion (sin necesidad de poner '')
+    ValorNuevo NUMBER(11,2) := &vn; -- Introducimos el valor nuevo de la cuenta
+    Comentario VARCHAR(45) := '&c'; -- Introducimos el comentario pertinente
     
 -- <- Fin Variables -> -- 
     
 -- <- Excepciones -> --
-    CuentaNoExiste EXCEPTION;
+    /* Necesitamos definir 3 excepciones:
+        Para controlar la inserción de cuentas ya existentes, poseemos ya le excepción DUP_VAL_ON_INDEX que se salta cuando se intenta insertar una cuenta con ID ya existente
+        Para controlar la actualización de cuentas que NO existen, definimos la excepción CuentaNoExiste
+        Para controlar la eliminación de cuentas que NO existen, definimos la excepción BorrarCuentaNoExiste
+        Para controlar realizar otro tipo de operaciones, definimos la excepción OperacionDesconocida
+    */
+    CuentaNoExiste EXCEPTION;  
     BorrarCuentaNoExiste EXCEPTION;
+    OperacionDesconocida EXCEPTION;
 -- <- Fin Excepciones -> --
     
-BEGIN
-    IF Operacion = 'i' OR Operacion = 'I' THEN
-        INSERT INTO CUENTAS VALUES(IDC,ValorNuevo);
-        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se inserto la cuenta con exito',SYSDATE);
+-- <-- Comienzo del cuerpo del script --> --
+BEGIN 
+
+    -- <-- Bloque para la operacion Insertar Cuenta --> --
+    
+    IF Operacion = 'i' OR Operacion = 'I' THEN -- Si la operacion elegida es i o I (Insertar)
+    
+        INSERT INTO CUENTAS VALUES(IDC,ValorNuevo); -- Insertamos a la tabla cuentas la nueva cuenta
+        /* Notese que si intentasemos insertar una cuenta ya existente, la excepción DUP_VAL_ON_INDEX
+           seria levantada y no continuaremos mas allá de esta linea*/
+        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se inserto la cuenta con exito',SYSDATE); -- Guardamos la operacion realizada en la tabla ACCIONES
         
-    ELSIF Operacion = 'a' OR Operacion = 'A' THEN
+    -- <-- FIN bloque para la operacion Insertar Cuenta --> --    
+   
+    -- <-- Bloque para la operacion Actualizar Cuenta --> --
+   
+    ELSIF Operacion = 'a' OR Operacion = 'A' THEN -- Si la operacion elegida es a o A (Actualizar)
+        -- Intentamos actualizar el valor obtenido en la cuenta seleccionada
         UPDATE CUENTAS
         SET Valor = ValorNuevo
         WHERE IDCuenta = IDC;
         
-        IF SQL%ROWCOUNT = 0 THEN
-            RAISE CuentaNoExiste;
+        /* Notese que si la cuenta no existiese, la cantidad de ROWS (filas) afectadas seria 0... */
+        
+        IF SQL%ROWCOUNT = 0 THEN -- Si la cantidad de ROWS (filas) afectadas es 0
+            RAISE CuentaNoExiste; -- Levantamos la excepcion de CuentaNoExiste
         END IF;
         
-        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se actualizo la cuenta con exito',SYSDATE);
+        /* Si la excepcion se levantase, no continuariamos mas allá de esta linea */
         
-    ELSIF Operacion = 'b' OR Operacion = 'B' THEN
-        DELETE FROM CUENTAS
-        WHERE IDCuenta = IDC;
+        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se actualizo la cuenta con exito',SYSDATE); -- Guardamos la operacion realizada en la tabla ACCIONES
         
-        IF SQL%ROWCOUNT = 0 THEN
-            RAISE BorrarCuentaNoExiste;
+    -- <-- FIN bloque para la operacion Actualizar Cuenta --> --
+    
+    -- <-- Bloque para la operacion Borrar Cuenta --> --
+        
+    ELSIF Operacion = 'b' OR Operacion = 'B' THEN -- Si la operacion elegida es b o B (Insertar)
+        DELETE FROM CUENTAS -- Borramos de la tabla CUENTAS
+        WHERE IDCuenta = IDC; -- La cuenta que tenga el ID proporcionado
+        
+        /* Notese que si la cuenta no existiese, la cantidad de ROWS (filas) afectadas seria 0... */
+        
+        IF SQL%ROWCOUNT = 0 THEN -- Si la cantidad de ROWS (filas) afectadas es 0
+            RAISE BorrarCuentaNoExiste;   -- Levantamos la excepcion de BorrarCuentaNoExiste
         END IF;
+        
+        /* Si la excepcion se levantase, no continuariamos mas allá de esta linea */
     
-        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se Borro la cuenta con exito',SYSDATE);
+        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se Borro la cuenta con exito',SYSDATE); -- Guardamos la operacion realizada en la tabla ACCIONES
+        
+    -- <-- FIN bloque para la operacion Borrar Cuenta --> --
     
+    -- <-- En el caso que la operacion no fuese alguna de las anteriores --> --
+    
+    ELSE  
+        raise OperacionDesconocidad; -- Levantamos la excepcion de OperacionDesconocidad
     END IF;
 
+-- <-- FIN del cuerpo del script --> --
+
+-- <-- Comienzo de tratamiento de excepciones --> --
 EXCEPTION
+
+    -- <-- Caso excepcion DUP_VAL_ON_INDEX (insertar una tupla con id de cuenta repetido) --> --
     WHEN DUP_VAL_ON_INDEX THEN
-        DBMS_OUTPUT.PUT_LINE('Error al insertar, ya existe la cuenta. Procedemos a actualizarla');
-        
-        UPDATE CUENTAS
+        /*Si estamos aqui es porque la cuenta existe, se podra actualizar siempre, no hay que tener encuenta otras excepciones */
+        DBMS_OUTPUT.PUT_LINE('Error al insertar, ya existe la cuenta. Procedemos a actualizarla'); -- Mostramos mensaje de error y procedemos a actualizar la cuenta
+        -- actualizamos el valor obtenido en la cuenta seleccionada
+        UPDATE CUENTAS  
         SET Valor = ValorNuevo
         WHERE IDCuenta = IDC;
         
-        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se actualizo la cuenta con exito',SYSDATE);
+        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se actualizo la cuenta con exito',SYSDATE); -- Guardamos la operacion realizada en la tabla ACCIONES
+    -- <-- FIN caso excepcion DUP_VAL_ON_INDEX (insertar una tupla con id de cuenta repetido) --> --
         
-        
-    WHEN BorrarCuentaNoExiste THEN 
-        DBMS_OUTPUT.PUT_LINE('Error al borrar, la cuenta no existe');
-        
+    -- <-- Caso excepcion CuentaNoExiste (actualizar una tupla con id de cuenta que no existe) --> --
     WHEN CuentaNoExiste THEN
+        /*Si estamos aqui es porque la cuenta NO existe, se podra insertar siempre, no hay que tener encuenta otras excepciones */
+        DBMS_OUTPUT.PUT_LINE('Error al actualizar, la cuenta no existe. Procedemos a insertarla'); -- Mostramos mensaje de error y procedemos a insertar la cuenta
+        INSERT INTO CUENTAS VALUES(IDC,ValorNuevo); --Insertamos cuenta en tabla CUENTAS
+        INSERT INTO ACCIONES VALUES(IDC,Operacion,ValorNuevo,'Se inserto la cuenta con exito',SYSDATE); -- Guardamos la operacion realizada en la tabla ACCIONES
+    -- <-- FIN caso excepcion CuentaNoExiste (actualizar una tupla con id de cuenta que no existe) --> --
+    
+    -- <-- Caso excepcion BorrarCuentaNoExiste (Borrar una tupla con id de cuenta que no existe) --> --
+    WHEN BorrarCuentaNoExiste THEN 
+        /* En este caso no es necesario hacer nada */
+        DBMS_OUTPUT.PUT_LINE('Error al borrar, la cuenta no existe'); -- Mostramos mensaje de error
+    -- <-- FIN caso excepcion BorrarCuentaNoExiste (Borrar una tupla con id de cuenta que no existe) --> --
+        
+    -- <-- Caso excepcion OperacionDesconocidad (Operacion desconocida) --> --
+    WHEN OperacionDesconocidad THEN
+        /* En este caso no es necesario hacer nada */
+        DBMS_OUTPUT.PUT_LINE('Error, la operacion solicitada no existe. Ningun cambio será realizado'); -- Mostramos mensaje de error
+    -- <-- FIN caso excepcion OperacionDesconocidad (Operacion desconocida) --> --
+    
+-- <-- FIN comienzo de tratamiento de excepciones --> --
 
-        DBMS_OUTPUT.PUT_LINE('Error al actualizar, la cuenta no existe. Procedemos a insertarla');
-        INSERT INTO CUENTAS VALUES
-        (
-            IDC,
-            ValorNuevo
-        );
-        INSERT INTO ACCIONES VALUES
-        (
-            IDC,
-            Operacion,
-            ValorNuevo,
-            'Se inserto la cuenta con exito',
-            SYSDATE
-        );
+-- <-- FIN del script --> --
 END;
 /
